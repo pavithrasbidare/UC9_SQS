481024220f910d29c688f5ca8647b0013b103d4f
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 14:05:10 +0530

    Update main.tf

--- terraform/modules/eventbridge/main.tf
+++ terraform/modules/eventbridge/main.tf
@@ -1,8 +1,8 @@
 module "eventbridge" {
-  source    = "terraform-aws-modules/eventbridge/aws"
-  version   = "3.14.3"  # Ensure you're using the correct version of the module
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "3.14.3"  # Ensure you're using the correct version of the module
 
-  event_bus_name = "my-event-bus1"  # Correct argument name for EventBus
+  bus_name = "my-event-bus1"  # Use bus_name as the argument for the EventBridge event bus name
 }
 
 output "event_bus_name" {
