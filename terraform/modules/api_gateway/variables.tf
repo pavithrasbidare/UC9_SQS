0cb0f5feec2c40730b750c1a79ba70ee9be9ec62
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:29:20 +0530

    Create variables.tf

---
+++ terraform/modules/api_gateway/variables.tf
@@ -0,0 +1,9 @@
+variable "lambda_arn" {
+  description = "Lambda ARN"
+  type        = string
+}
+
+variable "api_gateway_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
