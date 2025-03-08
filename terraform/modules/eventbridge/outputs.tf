654723227ace09612c24265a5b97ec55af518f70
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 14:49:51 +0530

    Update outputs.tf

--- terraform/modules/eventbridge/outputs.tf
+++ terraform/modules/eventbridge/outputs.tf
@@ -1,4 +1,3 @@
-# Ensure this output is present in the outputs.tf of the eventbridge module
 output "event_bus_arn" {
   value = aws_cloudwatch_event_bus.this.arn  # Reference the EventBridge ARN directly
 }
