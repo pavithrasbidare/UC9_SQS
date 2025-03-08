71e4cb834fe8b4955e9a74ac0314028a4fcce791
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 15:13:34 +0530

    Update main.tf

--- terraform/modules/eventbus/main.tf
+++ terraform/modules/eventbus/main.tf
@@ -1,6 +1,21 @@
 resource "aws_cloudwatch_event_bus" "example" {
   name = "example-event-bus"
 }
+resource "aws_sqs_queue" "gold_sqs" {
+  name = "gold-sqs"
+}
+
+resource "aws_sqs_queue" "silver_sqs" {
+  name = "silver-sqs"
+}
+
+output "gold_sqs_url" {
+  value = aws_sqs_queue.gold_sqs.url
+}
+
+output "silver_sqs_url" {
+  value = aws_sqs_queue.silver_sqs.url
+}
 
 output "event_bus_arn" {
   value = aws_cloudwatch_event_bus.example.arn
