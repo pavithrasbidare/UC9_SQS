355058ebbda671c7090c414d16dd1a73fe561e93
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:23:50 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+module "lambda" {
+  source      = "./modules/lambda"
+  lambda_name = "lambda-client1"
+  event_bus   = module.eventbridge.event_bus_name
+}
+
+module "api_gateway" {
+  source         = "./modules/api_gateway"
+  lambda_arn     = module.lambda.lambda_arn
+  api_gateway_name = "client-api"
+}
+
+module "sqs" {
+  source     = "./modules/sqs"
+  sqs_queue_1 = "sqs-gold1"
+  sqs_queue_2 = "sqs-silver1"
+}
+
+module "eventbridge" {
+  source = "./modules/eventbridge"
+}
+
+module "eventbridge_rules" {
+  source        = "./modules/eventbridge_rules"
+  event_bus     = module.eventbridge.event_bus_name
+}
