01f7817a97407c2a9a0661051ad116408c0e5204
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 17:19:38 +0530

    Update main.tf

--- terraform/modules/eventbus/main.tf
+++ terraform/modules/eventbus/main.tf
@@ -19,9 +19,9 @@ resource "aws_cloudwatch_event_rule" "gold_rule" {
   event_bus_name = aws_cloudwatch_event_bus.example.name
   event_pattern  = jsonencode({
     "source": ["insert_data_lambda"],
-    "detail-type": ["client-details"],
-    "detail": {
-      "client-type": ["gold"]
+  "detail-type": ["client-details"],
+  "detail": {
+    "client-type": ["gold1"]
     }
   })
 }
@@ -30,10 +30,10 @@ resource "aws_cloudwatch_event_rule" "silver_rule" {
   name           = "silver-rule"
   event_bus_name = aws_cloudwatch_event_bus.example.name
   event_pattern  = jsonencode({
-    "source": ["insert_data_lambdat"],
-    "detail-type": ["client-type"],
-    "detail": {
-      "client": ["silver"]
+    "source": ["insert_data_lambda"],
+  "detail-type": ["client-details"],
+  "detail": {
+    "client-type": ["silver1"]
     }
   })
 }
