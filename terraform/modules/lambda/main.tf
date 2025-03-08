9da697b2a786dae6c9191813e5e3d070aa3d5ece
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 15:57:35 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -4,16 +4,32 @@ data "archive_file" "insert_data_zip" {
   output_path = "${path.module}/lambda_functions/insert_data.zip"
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
 
 resource "aws_lambda_function" "insert_data_lambda" {
   function_name = var.function_name
-  role          = var.role_arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   filename      = data.archive_file.insert_data_zip.output_path
-  }
+}
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_dynamodb_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
