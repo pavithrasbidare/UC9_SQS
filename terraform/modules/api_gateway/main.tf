d69688b592d767d637d26ed8aedcf9d087fd2198
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:25:04 +0530

    Create main.tf

---
+++ terraform/modules/api_gateway/main.tf
@@ -0,0 +1,35 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name        = var.api_gateway_name
+  description = "API Gateway for triggering Lambda"
+}
+
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "trigger"
+}
+
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.root.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.post.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+}
+
+output "api_url" {
+  value = aws_api_gateway_deployment.api_deployment.invoke_url
+}
