3a6584f99bd3a5a250e7b94bf53b9144838e2c28
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:56:31 +0530

    Update main.tf

--- terraform/modules/eventbridge_rules/main.tf
+++ terraform/modules/eventbridge_rules/main.tf
@@ -1,35 +1,46 @@
-resource "aws_events_rule" "gold_clients_rule" {
-  name           = "gold-clients-rule"
-  event_bus_name = aws_events_event_bus.event_bus.name
+module "eventbridge_rules" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "3.14.3"
 
-  event_pattern = jsonencode({
-    source     = ["lambda-client1"]
-    "detail-type" = ["client-details"]
-    detail = {
-      "client-type" = ["gold"]
+  event_bus_name = module.eventbridge.event_bus_name
+
+  # Gold clients rule
+  rules = [
+    {
+      name = "gold-clients-rule"
+      event_pattern = jsonencode({
+        source = ["lambda-client1"]
+        "detail-type" = ["client-details"]
+        detail = {
+          "client-type" = ["gold"]
+        }
+      })
+      targets = [
+        {
+          arn = "arn:aws:sqs:us-west-2:123456789012:sqs-gold1"
+          id  = "gold-clients-target"
+        }
+      ]
     }
-  })
+  ]
 
-  target {
-    arn = "arn:aws:sqs:us-west-2:123456789012:sqs-gold1"
-    id  = "gold-clients-target"
-  }
-}
-
-resource "aws_events_rule" "silver_clients_rule" {
-  name           = "silver-clients-rule"
-  event_bus_name = aws_events_event_bus.event_bus.name
-
-  event_pattern = jsonencode({
-    source     = ["lambda-client1"]
-    "detail-type" = ["client-details"]
-    detail = {
-      "client-type" = ["silver"]
+  # Silver clients rule
+  rules = [
+    {
+      name = "silver-clients-rule"
+      event_pattern = jsonencode({
+        source = ["lambda-client1"]
+        "detail-type" = ["client-details"]
+        detail = {
+          "client-type" = ["silver"]
+        }
+      })
+      targets = [
+        {
+          arn = "arn:aws:sqs:us-west-2:123456789012:sqs-silver1"
+          id  = "silver-clients-target"
+        }
+      ]
     }
-  })
-
-  target {
-    arn = "arn:aws:sqs:us-west-2:123456789012:sqs-silver1"
-    id  = "silver-clients-target"
-  }
+  ]
 }
