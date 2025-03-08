4662f2ca33fa673119026f83f9b38e3f5c2fc9cd
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 16:14:55 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -4,26 +4,9 @@ data "archive_file" "insert_data_zip" {
   output_path = "${path.module}/lambda_functions/insert_data.zip"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_lambda_function" "insert_data_lambda" {
   function_name = var.function_name
-  role          = aws_iam_role.lambda_role.arn
+  role          = "arn:aws:iam::510278866235:role/lambda_exec_role"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   filename      = data.archive_file.insert_data_zip.output_path
