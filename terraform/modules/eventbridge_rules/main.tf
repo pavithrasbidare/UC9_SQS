51dc23b19a31285e8f6c985a757b159beeeacc55
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:48:33 +0530

    Update main.tf

--- terraform/modules/eventbridge_rules/main.tf
+++ terraform/modules/eventbridge_rules/main.tf
@@ -1,33 +1,35 @@
 resource "aws_events_rule" "gold_clients_rule" {
-  name        = "gold-clients"
-  event_bus_name = var.event_bus
+  name           = "gold-clients-rule"
+  event_bus_name = aws_events_event_bus.event_bus.name
+
   event_pattern = jsonencode({
-    source      = ["lambda-client1"],
-    "detail-type" = ["client-details"],
-    detail      = {
+    source     = ["lambda-client1"]
+    "detail-type" = ["client-details"]
+    detail = {
       "client-type" = ["gold"]
     }
   })
-  
+
   target {
-    arn = module.sqs.gold_queue_url
-    id  = "sqs-target-gold"
+    arn = "arn:aws:sqs:us-west-2:123456789012:sqs-gold1"
+    id  = "gold-clients-target"
   }
 }
 
 resource "aws_events_rule" "silver_clients_rule" {
-  name        = "silver-clients"
-  event_bus_name = var.event_bus
+  name           = "silver-clients-rule"
+  event_bus_name = aws_events_event_bus.event_bus.name
+
   event_pattern = jsonencode({
-    source      = ["lambda-client1"],
-    "detail-type" = ["client-details"],
-    detail      = {
+    source     = ["lambda-client1"]
+    "detail-type" = ["client-details"]
+    detail = {
       "client-type" = ["silver"]
     }
   })
 
   target {
-    arn = module.sqs.silver_queue_url
-    id  = "sqs-target-silver"
+    arn = "arn:aws:sqs:us-west-2:123456789012:sqs-silver1"
+    id  = "silver-clients-target"
   }
 }
