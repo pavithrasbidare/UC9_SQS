6298defd7f480ccda20554120281c0653102318e
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:43:34 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -27,8 +27,14 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_deployment" "api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
 }
+
+resource "aws_api_gateway_stage" "api_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+}
+
 
 output "api_url" {
   value = aws_api_gateway_deployment.api_deployment.invoke_url
