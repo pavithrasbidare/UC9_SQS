893fa71d97c2bb17bf212164559cce776e9fc8a3
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 15:17:03 +0530

    Update main.tf

--- terraform/modules/eventbus/main.tf
+++ terraform/modules/eventbus/main.tf
@@ -1,12 +1,45 @@
+provider "aws" {
+  region = "us-west-2"  # Specify your desired region
+}
+
 resource "aws_cloudwatch_event_bus" "example" {
   name = "example-event-bus"
 }
+
 resource "aws_sqs_queue" "gold_sqs" {
   name = "gold-sqs"
 }
 
 resource "aws_sqs_queue" "silver_sqs" {
   name = "silver-sqs"
+}
+
+resource "aws_cloudwatch_event_rule" "gold_rule" {
+  name        = "gold-rule"
+  event_bus_name = aws_cloudwatch_event_bus.example.name
+  event_pattern = jsonencode({
+    "source": ["gold-source"]
+  })
+}
+
+resource "aws_cloudwatch_event_rule" "silver_rule" {
+  name        = "silver-rule"
+  event_bus_name = aws_cloudwatch_event_bus.example.name
+  event_pattern = jsonencode({
+    "source": ["silver-source"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "gold_target" {
+  rule      = aws_cloudwatch_event_rule.gold_rule.name
+  target_id = "gold-sqs-target"
+  arn       = aws_sqs_queue.gold_sqs.arn
+}
+
+resource "aws_cloudwatch_event_target" "silver_target" {
+  rule      = aws_cloudwatch_event_rule.silver_rule.name
+  target_id = "silver-sqs-target"
+  arn       = aws_sqs_queue.silver_sqs.arn
 }
 
 output "gold_sqs_url" {
