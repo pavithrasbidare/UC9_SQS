70d71a291b161e7f27a810179c242f70e5017e51
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 13:28:14 +0530

    Update variables.tf

--- terraform/modules/sqs/variables.tf
+++ terraform/modules/sqs/variables.tf
@@ -1,1 +1,10 @@
 
+variable "sqs_queue_1" {
+  description = "Name of the first SQS queue"
+  type        = string
+}
+
+variable "sqs_queue_2" {
+  description = "Name of the second SQS queue"
+  type        = string
+}
