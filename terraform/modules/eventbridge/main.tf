69050b43a632d61c960dbb41ef633c18cb339228
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-08 14:08:53 +0530

    Update main.tf

--- terraform/modules/eventbridge/main.tf
+++ terraform/modules/eventbridge/main.tf
@@ -5,6 +5,6 @@ module "eventbridge" {
   bus_name = "my-event-bus1"  # Use bus_name as the argument for the EventBridge event bus name
 }
 
-output "event_bus_name" {
-  value = module.eventbridge.event_bus_name
+output "bus_name" {
+  value = module.eventbridge.bus_name  # Corrected output variable name to bus_name
 }
