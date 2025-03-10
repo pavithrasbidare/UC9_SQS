44db51856ff95caf76d97d428c236caae4264563
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-10 16:10:51 +0530

    Update main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -6,7 +6,7 @@ data "archive_file" "insert_data_zip" {
 
 resource "aws_lambda_function" "insert_data_lambda" {
   function_name = var.function_name
-  role          = "arn:aws:iam::510278866235:role/lambda_exec_role"
+  role          = "arn:aws:iam::302263075199:role/lambda-execution-role"
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
   filename      = data.archive_file.insert_data_zip.output_path
